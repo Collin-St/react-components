88dfd2efa347f0b3afb43faf655c83b0556b4e57
collin-st <collin.street.dr@gmail.com>
2018-11-26 15:05:32 -0800

    Progress

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 // TODO
+// var groceryItems = ['apples', 'oranges'];
 var GroceryList = (props) => (
   <ul>
-    <li>apples</li>
-    <li>oranges</li>
+    <li>{props.todos[0]}</li>
+    <li>{props.todos[1]}</li>
   </ul>
 );
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+var App = () => (
+  <div>
+    <h2>My Todo List</h2>
+    <GroceryList todos={['apples', 'oranges']}/>
+  </div>
+);
+
+ReactDOM.render(
+  <GroceryList />, 
+  document.getElementById('app')
+);
